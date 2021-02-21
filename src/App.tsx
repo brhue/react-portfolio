02bdac3eb59feb5f18a9e90d1df9d1db0e515909
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-21 12:06:21 -0500

    Setup react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>App!</h1>
-    </div>
+    <Router>
+      {/* header here */}
+      <main>
+        <Switch>
+          <Route path="/projects">
+            <h1>Projects</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </main>
+      {/* footer here */}
+    </Router>
   );
 }
 
