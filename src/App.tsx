b8e8f6b990e8a1f65d90018d9c343d09e6da7aa8
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-21 15:35:06 -0500

    Add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import "./App.css";
 
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 
@@ -21,7 +22,7 @@ function App() {
           </Switch>
         </div>
       </main>
-      {/* footer here */}
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="Footer">
+      <div className="container">
+        <small>Made with <span className="heart">&hearts;</span> by Bradley Donahue.</small>
+      </div>
+    </footer>
+  );
+}
