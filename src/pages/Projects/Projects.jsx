17a5c6c4f982d430b2adcb78047a43371ce5bb24
Bradley Donahue <donahue.bradley@gmail.com>
2021-02-22 19:21:00 -0500

    Refactor

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,51 +1,7 @@
 import "./Projects.css";
 
 import Project from "../../components/Project/Project";
-import jsQuizImage from "../../images/code-quiz.gif";
-import weatherImage from "../../images/weather.png";
-import cobaltImage from "../../images/cobalt.png";
-import literateImage from "../../images/literate-listener.png";
-import employeeImage from "../../images/employee-manager.png";
-import workoutImage from "../../images/workout.png";
-
-const projects = [
-  {
-    title: "Cobalt Canvas",
-    github: "https://github.com/brhue/Cobalt",
-    deployed: "https://cobalt-canvas.herokuapp.com/",
-    image: cobaltImage,
-  },
-  {
-    title: "Literate Listener",
-    github: "https://github.com/brhue/literate-listener",
-    deployed: "https://brhue.github.io/literate-listener/",
-    image: literateImage,
-  },
-  {
-    title: "Employee Manager",
-    github: "https://github.com/brhue/employee-tracker",
-    deployed: "",
-    image: employeeImage,
-  },
-  {
-    title: "Workout Tracker",
-    github: "https://github.com/brhue/nosql-workout-tracker",
-    deployed: "https://shielded-plains-15530.herokuapp.com/",
-    image: workoutImage,
-  },
-  {
-    title: "Weather Dashboard",
-    github: "https://github.com/brhue/weather-dashboard",
-    deployed: "https://brhue.github.io/weather-dashboard/",
-    image: weatherImage,
-  },
-  {
-    title: "JS Code Quiz",
-    github: "https://github.com/brhue/js-code-quiz",
-    deployed: "https://brhue.github.io/js-code-quiz",
-    image: jsQuizImage,
-  },
-];
+import { projects } from "../../projects";
 
 export default function Projects() {
   return (
---
+++ src/projects.js
@@ -0,0 +1,51 @@
+import jsQuizImage from "./images/code-quiz.gif";
+import weatherImage from "./images/weather.png";
+import cobaltImage from "./images/cobalt.png";
+import literateImage from "./images/literate-listener.png";
+import employeeImage from "./images/employee-manager.png";
+import workoutImage from "./images/workout.png";
+
+export const projects = [
+  {
+    id: 1,
+    title: "Cobalt Canvas",
+    github: "https://github.com/brhue/Cobalt",
+    deployed: "https://cobalt-canvas.herokuapp.com/",
+    image: cobaltImage,
+  },
+  {
+    id: 2,
+    title: "Literate Listener",
+    github: "https://github.com/brhue/literate-listener",
+    deployed: "https://brhue.github.io/literate-listener/",
+    image: literateImage,
+  },
+  {
+    id: 3,
+    title: "Employee Manager",
+    github: "https://github.com/brhue/employee-tracker",
+    deployed: "",
+    image: employeeImage,
+  },
+  {
+    id: 4,
+    title: "Workout Tracker",
+    github: "https://github.com/brhue/nosql-workout-tracker",
+    deployed: "https://shielded-plains-15530.herokuapp.com/",
+    image: workoutImage,
+  },
+  {
+    id: 5,
+    title: "Weather Dashboard",
+    github: "https://github.com/brhue/weather-dashboard",
+    deployed: "https://brhue.github.io/weather-dashboard/",
+    image: weatherImage,
+  },
+  {
+    id: 6,
+    title: "JS Code Quiz",
+    github: "https://github.com/brhue/js-code-quiz",
+    deployed: "https://brhue.github.io/js-code-quiz",
+    image: jsQuizImage,
+  },
+];
